9d01d139d3d7966382e9503b92d93941de0be8f4
Josue Silva de Azevedo de Carvalho <115432564+Josuerx12@users.noreply.github.com>
2023-09-14 00:41:41 -0300

    update api link

--- src/utils/apiConnect.ts
+++ src/utils/apiConnect.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://3.139.75.39:8080/",
+  baseURL: "https://backendtaskmanager.vercel.app/",
   headers: { "Content-Type": "application/json" },
 });
