197e9a0e133f474e12d5506e9ae887a572ef43e7
Josue Silva de Azevedo de Carvalho <115432564+Josuerx12@users.noreply.github.com>
2023-09-15 15:30:06 -0300

    Atualizar o apiConnect.ts

--- src/utils/apiConnect.ts
+++ src/utils/apiConnect.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://backendtaskmanager.vercel.app/",
+  baseURL: "https://apitaskm.josuecarvalho.cloud/",
   headers: { "Content-Type": "application/json" },
 });
